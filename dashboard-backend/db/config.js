7ba262b731c01a1152352111900df2242a4b4884
riyaz-shaikh <qureshiriyaz154@gmail.com>
2023-03-11 11:19:03 +0530

    e-commerce-dashoboard

---
+++ dashboard-backend/db/config.js
@@ -0,0 +1,9 @@
+const mongoose= require('mongoose');
+mongoose.connect("mongodb://localhost:27017/e-commerce", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    family: 4,
+    
+})
+
+
